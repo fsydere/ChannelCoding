72fb501ae38a4f6a9a43eeb847759f3cdab3a0e6
FSY <fsydere99@gmail.com>
2024-03-17 22:47:36 +0300

    some updates

--- Hamming/Hamming_7_4_Encoder.m
+++ Hamming/Hamming_7_4_Encoder.m
@@ -1,4 +1,4 @@
-function [CodedM] = Hamming_7_4_Encoder(input)
+function [CodedData] = Hamming_7_4_Encoder(input)
     %UNTITLED3 Summary of this function goes here
     %   (7,4) Hamming Decoder
     len = length(input);
@@ -7,12 +7,12 @@ function [CodedM] = Hamming_7_4_Encoder(input)
          0 0 1 0 1 1 0;
          0 0 0 1 0 1 1];
     
-    CodedM = zeros(1,((len/4)*3)+len) == 0;
+    CodedData = zeros(1,((len/4)*3)+len) == 0;
     
     lastptr = 1;
     
     for iter = 1:4:len
-        CodedM(lastptr:lastptr+6) = (mod(input(iter:iter+3)*G,2) == 1);
+        CodedData(lastptr:lastptr+6) = (mod(input(iter:iter+3)*G,2) == 1);
         lastptr = lastptr+7;
     end
 end
